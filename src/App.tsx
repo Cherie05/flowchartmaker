c27eb6749023fdc4316bb27cd896b1d5a650b346
arunvpp <arunvpp24@gmail.com>
2026-06-08 14:30:25 +0530

    fix: remove unused Navigate import to fix ESLint in CI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { LandingPage } from './pages/LandingPage';
 import { AuthPage } from './pages/AuthPage';
